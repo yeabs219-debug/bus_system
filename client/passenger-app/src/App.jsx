faee3334266dd1ebca56ba04335b3b491a9eb303
Yeabs219-debug <yeabs219@email.com>
2026-06-26 16:58:48 +0300

    connect to app.jsx

--- client/passenger-app/src/App.jsx
+++ client/passenger-app/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import SearchPage from './pages/SearchPage';
 import RoutePage from './pages/RoutePage';
+import ScanPage from './pages/ScanPage';
+import TicketPage from './pages/TicketPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SearchPage />} />
       <Route path="/route/:routeId" element={<RoutePage />} />
+      <Route path="/scan" element={<ScanPage />} />
+      <Route path="/ticket" element={<TicketPage />} />
     </Routes>
   );
 }
